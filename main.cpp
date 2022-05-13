1e737b75f7095aee184a0cb06f66e1c8ad441b81
Nguyen Duy Quang <nguyenduyquang2002@gmail.com>
2022-05-13 21:03:26 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class SinhVien
+{
+private:
+    /* data */
+public:
+    SinhVien(/* args */);
+    ~SinhVien();
+    int QLSinhvien();
+};
+
+SinhVien::SinhVien(/* args */)
+{
+}
+
+SinhVien::~SinhVien()
+{
+}
